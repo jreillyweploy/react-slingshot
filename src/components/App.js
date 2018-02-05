e7626dd39af42a667a4ac8c3842815929d90b262
James Reilly <jreilly@zendesk.com>
2018-02-05 13:47:39 +0000

    Add ToDoPage container

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Switch, NavLink, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import FuelSavingsPage from './containers/FuelSavingsPage';
+import ToDoPage from './containers/ToDoPage';
 import AboutPage from './AboutPage';
 import NotFoundPage from './NotFoundPage';
 
@@ -13,20 +14,13 @@ import NotFoundPage from './NotFoundPage';
 
 class App extends React.Component {
   render() {
-    const activeStyle = { color: 'blue' };
     return (
       <div>
-        <div>
-          <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
-          {' | '}
-          <NavLink to="/fuel-savings" activeStyle={activeStyle}>Demo App</NavLink>
-          {' | '}
-          <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
-        </div>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/fuel-savings" component={FuelSavingsPage} />
           <Route path="/about" component={AboutPage} />
+          <Route path="/todo" component={ToDoPage} />
           <Route component={NotFoundPage} />
         </Switch>
       </div>
---
+++ src/components/containers/ToDoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class ToDoPage extends React.Component {
+  render() {
+    return (
+      <h1>I am a TODO page</h1>
+    );
+  }
+}
+
+ToDoPage.propTypes = {};
+
+export default ToDoPage;
