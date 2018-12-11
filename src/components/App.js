ccdc47af291aa1d9d678105bcf8f22202fb0b254
James Reilly <james.reilly@weployapp.com>
2018-12-11 21:18:21 +1100

    Add antd design components

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import NotFoundPage from './NotFoundPage';
 
+import 'antd/dist/antd.css';
+
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
 // component at the top-level.
